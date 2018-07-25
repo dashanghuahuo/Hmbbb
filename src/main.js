bb089933b793c0b84b89872c3b3b2fc861005d00
feng <feng@qq.com>
2018-07-25 21:16:12 +0800

    完成导航和主页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import index from './components/index.vue'
+
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes :[
+        { path: '/', component: index },
+        { path: '/index', component: index },
+        
+    ]
+  })
+  
+
+  import './assets/statics/site/css/style.css';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+    el:'#app',
+  render: h => h(App),
+  router
+})
